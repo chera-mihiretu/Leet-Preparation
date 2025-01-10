de598cce1180281d4837cf0e0b35e1d6f6b2cdd9
chera-mihiretu <cheramihiretucm@gmail.com>
2025-01-10 23:32:40 +0300

    subsequence with decreasing adjecent

---
+++ Longest_Subsequence_With_decreasing_adjecent_difference.py
@@ -0,0 +1,23 @@
+from typing import *
+
+class Solution:
+    def longestSubsequence(self, nums: List[int]) -> int:
+        M = max(nums) - min(nums)
+        n = len(nums)
+        dp = [[0] * (M + 1) for _ in range(2 * M + max(nums) + 2)]
+        res = 1
+        for i in range(n - 1, -1, -1):
+            curr = [0] * (M + 1)
+            l = 1
+            for diff in range(0, M + 1):
+                l = max(l, 1 + dp[nums[i] - diff][diff], 1 + dp[nums[i] + diff][diff])
+                curr[diff] = l
+                res = max(res, l)
+            for diff in range(0, M + 1):
+                dp[nums[i]][diff] = max(dp[nums[i]][diff], curr[diff])
+        
+        return res
+    
+x = Solution()
+
+print(x.longestSubsequence([5, 10, 8, 20, 10, 20]))
